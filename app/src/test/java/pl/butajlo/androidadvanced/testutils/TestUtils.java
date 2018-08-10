30ef34b01b704181302be567b9cb7da6db5f6ef9
Jarosław Butajło <jarekbutek@gmail.com>
2018-08-10 22:50:17 +0200

    Unit Tests: TestUtils - class used for testing serializing/deserializing objects from JSON

---
+++ app/src/test/java/pl/butajlo/androidadvanced/testutils/TestUtils.java
@@ -0,0 +1,65 @@
+package pl.butajlo.androidadvanced.testutils;
+
+import com.squareup.moshi.Moshi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+
+import pl.butajlo.androidadvanced.models.AdapterFactory;
+import pl.butajlo.androidadvanced.models.ZoneDateTimeAdapter;
+
+public class TestUtils {
+
+    private static TestUtils INSTANCE = new TestUtils();
+
+    private static final Moshi TEST_MOSHI = initializeMoshi();
+
+
+    private TestUtils() {}
+
+    public static <T> T loadJson(String path, Type type) {
+        try {
+            String json = getFileString(path);
+            //noinspection unchecked
+            return (T) TEST_MOSHI.adapter(type).fromJson(json);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not deserialize: " + path + " into type " + type);
+        }
+    }
+
+    public static <T> T loadJson(String path, Class<T> tClass) {
+        try {
+            String json = getFileString(path);
+            //noinspection unchecked
+            return (T) TEST_MOSHI.adapter(tClass).fromJson(json);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not deserialize: " + path + " into type " + tClass);
+        }
+    }
+
+    private static String getFileString(String path) {
+        try {
+            StringBuilder sb = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    INSTANCE.getClass().getClassLoader().getResourceAsStream(path)));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not read from resource at: " + path);
+        }
+
+    }
+
+    private static Moshi initializeMoshi() {
+        return new Moshi.Builder()
+                .add(AdapterFactory.create())
+                .add(new ZoneDateTimeAdapter())
+                .build();
+    }
+
+}
