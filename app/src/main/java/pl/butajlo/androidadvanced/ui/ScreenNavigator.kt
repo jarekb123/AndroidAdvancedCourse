223be8bd42bc7d0d5b6caf05cf7535071ed1d460
Jarosław Butajło <jarekbutek@gmail.com>
2018-09-17 22:02:25 +0200

    rewrite ScreenNavigator to Kotlin

---
+++ app/src/main/java/pl/butajlo/androidadvanced/ui/ScreenNavigator.kt
@@ -0,0 +1,20 @@
+package pl.butajlo.androidadvanced.ui
+
+import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.Router
+
+interface ScreenNavigator {
+
+    fun initWithRouter(router: Router, rootScreen: Controller)
+
+    /**
+     * Pop the actual screen
+     * @return true if the screen was popped, false if not
+     */
+    fun pop(): Boolean
+
+    /**
+     * It is used to clean the router, if the activity is destroyed
+     */
+    fun clear()
+}
